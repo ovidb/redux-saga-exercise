e2be6acd3cf3f2ae8260ac1716bd45cddae796c0
Richard Moss <richard@englishtutorslive.com>
2017-07-13 16:13:43 +0100

    Deletes tests from actions for ex

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -1,16 +1,8 @@
 import {
   receiveSession,
   fetchingBeers,
-  receiveBeers,
-  setCurrentBeerIndex,
-  fetchBeers,
-  startSession,
   RECEIVE_SESSION,
-  FETCHING_BEERS,
-  RECEIVE_BEERS,
-  CURRENT_BEER_INDEX,
-  FETCH_BEERS,
-  START_SESSION
+  FETCHING_BEERS
 } from './'
 
 it(`Returns an Action type ${RECEIVE_SESSION}`, () => {
@@ -25,25 +17,4 @@ it(`Returns an Action type ${FETCHING_BEERS}`, () => {
     isFetching: true
   }
   expect(fetchingBeers(isFetching)).toMatchSnapshot()
-})
-
-it(`Returns an Action type ${RECEIVE_BEERS}`, () => {
-  const beers = {
-    data: [],
-    isFetching: false
-  }
-  expect(receiveBeers(beers)).toMatchSnapshot()
-})
-
-it(`Returns an Action type ${CURRENT_BEER_INDEX}`, () => {
-  const currentIndex = 1
-  expect(setCurrentBeerIndex(currentIndex)).toMatchSnapshot()
-})
-
-it(`Returns an Action type ${FETCH_BEERS}`, () => {
-  expect(fetchBeers()).toMatchSnapshot()
-})
-
-it(`Returns an Action type ${START_SESSION}`, () => {
-  expect(startSession()).toMatchSnapshot()
 })
