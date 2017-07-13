424728945b5c67540a858aa389f3e5c324a34ab9
Richard Moss <richard@englishtutorslive.com>
2017-07-13 11:02:06 +0100

    Adds test for api

--- src/api/index.spec.js
+++ src/api/index.spec.js
@@ -1,5 +1,33 @@
 import * as api from './index'
+import { API_BASE_URL } from '../config'
 
-it('should startSession', () => {
-  
+const mockFetch = (data) => {
+  global.fetch = jest.fn()
+
+  fetch.mockImplementation(
+    (params) => Promise.resolve({
+      json: () => ({
+        data,
+        params
+      })
+    })
+  )
+}
+
+
+describe('#API', () => {
+  const session = {
+    "id": "reactJSacademy"
+  }
+  let data
+  data = session
+  beforeAll(() => {
+    mockFetch(data)
+  })
+
+  it('startSession()', async () => {
+    const response = await api.startSession()
+    console.log(response)
+    expect(response.data).toEqual(data)
+  })
 })
