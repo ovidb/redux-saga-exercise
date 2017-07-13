641476fa437ee0c429a447c5fecafadd34fe000c
Richard Moss <richard@englishtutorslive.com>
2017-07-13 12:36:33 +0100

    completes tests for actions

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -1,13 +1,49 @@
-import { receiveBeers, RECEIVE_BEERS } from './'
+import {
+  receiveSession,
+  fetchingBeers,
+  receiveBeers,
+  setCurrentBeerIndex,
+  fetchBeers,
+  startSession,
+  RECEIVE_SESSION,
+  FETCHING_BEERS,
+  RECEIVE_BEERS,
+  CURRENT_BEER_INDEX,
+  FETCH_BEERS,
+  START_SESSION
+} from './'
 
+it(`Returns an Action type ${RECEIVE_SESSION}`, () => {
+  const session = {
+    "id": "reactJSacademy"
+  }
+  expect(receiveSession(session)).toMatchSnapshot()
+})
+
+it(`Returns an Action type ${FETCHING_BEERS}`, () => {
+  const isFetching = {
+    isFetching: true
+  }
+  expect(fetchingBeers(isFetching)).toMatchSnapshot()
+})
 
 it(`Returns an Action type ${RECEIVE_BEERS}`, () => {
   const beers = {
-    "data": [],
-    "isFetching": false
+    data: [],
+    isFetching: false
   }
-  expect(receiveBeers(beers)).toEqual({
-    type: RECEIVE_BEERS,
-    beers
-  })
+  expect(receiveBeers(beers)).toMatchSnapshot()
+})
+
+it(`Returns an Action type ${CURRENT_BEER_INDEX}`, () => {
+  const currentIndex = 1
+  expect(setCurrentBeerIndex(currentIndex)).toMatchSnapshot()
+})
+
+it(`Returns an Action type ${FETCH_BEERS}`, () => {
+  expect(fetchBeers()).toMatchSnapshot()
+})
+
+it(`Returns an Action type ${START_SESSION}`, () => {
+  expect(startSession()).toMatchSnapshot()
 })
