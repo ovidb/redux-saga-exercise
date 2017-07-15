88a9152b9093c09c9f4ecbfa3e1c7ebc04d70540
alexlbr <alex.lobera@gmail.com>
2017-07-15 15:24:25 +0100

    refactored reducer test

--- src/reducers/beers.spec.js
+++ src/reducers/beers.spec.js
@@ -1,5 +1,5 @@
 import beers from './beers'
-import { RECEIVE_BEERS, FETCHING_BEERS } from '../actions'
+import { RECEIVE_BEERS, FETCHING_BEERS, receiveBeers, fetchingBeers } from '../actions'
 
 describe('#Reducer: Beers', () => {
   it('Returns default state', () => {
@@ -9,13 +9,13 @@ describe('#Reducer: Beers', () => {
     expect(beers({ data: ["data"], isFetching: false }, { type: 'another_action'})).toEqual({ data: ["data"], isFetching: false })
   })
   it(`When action ${RECEIVE_BEERS} returns state`, () => {
-    expect(beers({ data: ["data"], isFetching: false }, { type: RECEIVE_BEERS, beers: ["my beer"] })).toEqual({
+    expect(beers({ data: ["data"], isFetching: false }, receiveBeers(["my beer"]) )).toEqual({
       data: ["data", "my beer"],
       isFetching: false
     })
   })
   it(`When action ${FETCHING_BEERS} returns state`, () => {
-    expect(beers({ data: ["data"], isFetching: false }, { type: FETCHING_BEERS, isFetching: true })).toEqual({
+    expect(beers({ data: ["data"], isFetching: false }, fetchingBeers(true))).toEqual({
       data: ["data"],
       isFetching: true
     })
